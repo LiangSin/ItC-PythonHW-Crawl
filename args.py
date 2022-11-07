8ee302b6d324d27374f686f03ee8e4f899bd1d40
Liang <sunny990284@gmail.com>
2022-11-07 15:29:27 +0800

    fix argument format

--- args.py
+++ args.py
@@ -4,9 +4,9 @@ from datetime import datetime
 #parse the inputs(start_date end_date output)
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('start_date', type = str)
-    parser.add_argument('end_date', type = str)
-    parser.add_argument('output', type = str)
+    parser.add_argument('--start-date', type = str)
+    parser.add_argument('--end-date', type = str)
+    parser.add_argument('--output', type = str)
     args = parser.parse_args()
     args.start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
     args.end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
